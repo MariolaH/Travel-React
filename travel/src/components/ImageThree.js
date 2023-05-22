0174ca2a5c7776d3b64dd332c50a07419287ae94
MariolaH <mariolahullings@gmail.com>
2023-05-22 18:00:22 +0000

    updates

--- travel/src/components/ImageThree.js
+++ travel/src/components/ImageThree.js
@@ -1,10 +1,10 @@
 import { Parallax } from "react-parallax";
-import Dubrovnik3 from "../images/Dubrovnik3.png";
+import Dubrovnik3 from "../images/Dubrovnik3.jpeg";
 
 const ImageThree = () => (
   <Parallax className="image" bgImage={Dubrovnik3} strength={200}>
     <div className="content">
-      <span className="img-text">A Trip to Croatia</span>
+      {/* <span className="img-text">A Trip to Croatia</span> */}
     </div>
   </Parallax>
 );
--- travel/src/components/ImageTwo.js
+++ travel/src/components/ImageTwo.js
@@ -4,7 +4,7 @@ import Dubrovnik2 from "../images/Dobrovink2.jpeg";
 const ImageTwo = () => (
   <Parallax className="image" bgImage={Dubrovnik2} strength={200}>
     <div className="content">
-      <span className="img-text">A Trip to Croatia</span>
+      {/* <span className="img-text">A Trip to Croatia</span> */}
     </div>
   </Parallax>
 );
