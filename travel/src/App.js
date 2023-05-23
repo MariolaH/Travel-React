5d082524a032e4adeae2295340498719837f59c7
MariolaH <mariolahullings@gmail.com>
2023-05-23 16:54:18 +0000

    added instagram icon and link

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,27 +1,24 @@
-import './App.css';
-import ImageFour from './components/ImageFour';
-
-import ImageOne from './components/ImageOne';
-import ImageThree from './components/ImageThree';
-import ImageTwo from './components/ImageTwo';
-import { TextBox } from './components/TextBox';
-import { TextBox3 } from './components/TextBox3';
-import { TextBox5 } from './components/TextBox5';
-import { TextBox2 } from './components/Textbox2';
-
-
+import "../src/index.css";
+import ImageFour from "./components/ImageFour";
+import ImageOne from "./components/ImageOne";
+import ImageThree from "./components/ImageThree";
+import ImageTwo from "./components/ImageTwo";
+import { TextBox } from "./components/TextBox";
+import { TextBox3 } from "./components/TextBox3";
+import { TextBox5 } from "./components/TextBox5";
+import { TextBox2 } from "./components/Textbox2";
 
 function App() {
   return (
     <div>
-    <ImageOne />
-    <TextBox />
-    <ImageTwo />
-    <TextBox2 />
-    <ImageThree />
-    <TextBox3 />
-    <ImageFour />
-    <TextBox5 />
+      <ImageOne />
+      <TextBox />
+      <ImageTwo />
+      <TextBox2 />
+      <ImageThree />
+      <TextBox3 />
+      <ImageFour />
+      <TextBox5 />
     </div>
   );
 }
--- travel/src/components/TextBox5.js
+++ travel/src/components/TextBox5.js
@@ -4,6 +4,14 @@ export const TextBox5 = () => {
   return (
     <div className="text-box2">
       <p>Exciting updates coming soon!</p>
+
+      <a
+        rel="noreferrer"
+        target="_blank"
+        href="https://www.instagram.com/out_livin_the_dream/"
+      >
+        <img src="./IMG/instagram.svg" alt="Linkedin Logo" />
+      </a>
     </div>
   );
 };
