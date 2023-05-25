24acd5d58eb1f93f15d56123449fe3d5887a5929
MariolaH <mariolahullings@gmail.com>
2023-05-25 15:13:36 +0000

    changed image size

--- travel/src/components/ImageFive.js
+++ travel/src/components/ImageFive.js
@@ -2,7 +2,7 @@ import { Parallax } from "react-parallax";
 import Dubrovnik5 from "../images/cliff bar2.png";
 
 const ImageFive = () => (
-  <Parallax className="image" bgImage={Dubrovnik5} strength={400} bgImageAlt="Image of a cliff bar outside the walls of Dobrovnik"></Parallax>
+  <Parallax className="image" bgImage={Dubrovnik5} strength={250} bgImageAlt="Image of a cliff bar outside the walls of Dobrovnik"></Parallax>
 );
 
 export default ImageFive;
