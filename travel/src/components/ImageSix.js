9a945494e2471a4d82c4424b0a6b554db019358f
MariolaH <mariolahullings@gmail.com>
2023-05-25 14:29:49 +0000

    scroll update

--- travel/src/components/ImageSix.js
+++ travel/src/components/ImageSix.js
@@ -2,7 +2,7 @@ import { Parallax } from "react-parallax";
 import Food from "../images/Food.png";
 
 const ImageSix = () => (
-  <Parallax className="image" bgImage={Food} strength={250} bgImageAlt="Image of fresh seafood, potatoes and a salad"></Parallax>
+  <Parallax className="image" bgImage={Food} strength={500} bgImageAlt="Image of fresh seafood, potatoes and a salad"></Parallax>
 );
 
 export default ImageSix;
