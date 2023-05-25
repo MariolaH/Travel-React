3e2f0c74278814efd9ec3b66f508c7e0b13f2ebe
MariolaH <mariolahullings@gmail.com>
2023-05-25 19:59:37 +0000

    changed strength

--- travel/src/components/ImageOne.js
+++ travel/src/components/ImageOne.js
@@ -2,7 +2,7 @@ import { Parallax } from "react-parallax";
 import Dubrovnik from "../images/D1.jpeg";
 
 const ImageOne = () => (
-  <Parallax className="image" bgImage={Dubrovnik} strength={400} bgImageAlt="Image of Dubrovnik from the top of the walls">
+  <Parallax className="image" bgImage={Dubrovnik} strength={200} bgImageAlt="Image of Dubrovnik from the top of the walls">
     <div className="content">
       <span className="img-text">Croatia</span>
     </div>
--- travel/src/components/ImageSix.js
+++ travel/src/components/ImageSix.js
@@ -2,7 +2,7 @@ import { Parallax } from "react-parallax";
 import Food from "../images/Food.png";
 
 const ImageSix = () => (
-  <Parallax className="image" bgImage={Food} strength={400} bgImageAlt="Image of fresh seafood, potatoes and a salad"></Parallax>
+  <Parallax className="image" bgImage={Food} strength={200} bgImageAlt="Image of fresh seafood, potatoes and a salad"></Parallax>
 );
 
 export default ImageSix;
