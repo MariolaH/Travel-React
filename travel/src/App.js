544ad82a4cdfcd5438eddd6233f897018754cdee
MariolaH <mariolahullings@gmail.com>
2023-05-30 14:48:22 +0000

    changed scroll strength

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,32 +1,11 @@
 import "../src/index.css";
-import ImageFour from "./components/ImageFour";
-import ImageOne from "./components/ImageOne";
-import ImageThree from "./components/ImageThree";
-import ImageTwo from "./components/ImageTwo";
-import ImageFive from "./components/ImageFive";
-import { TextBox } from "./components/TextBox";
-import { TextBox3 } from "./components/TextBox3";
-import { TextBox5 } from "./components/TextBox5";
-import { TextBox2 } from "./components/Textbox2";
-import { TextBox4 } from "./components/TextBox4";
-import { TextBox6 } from "./components/TextBox6";
-import ImageSix from "./components/ImageSix";
+import Croatia from "./components/Croatia";
 
 function App() {
   return (
     <div>
-      <ImageOne />
-      <TextBox />
-      <ImageTwo />
-      <TextBox2 />
-      <ImageThree />
-      <TextBox3 />
-      <ImageFour />
-      <TextBox4 />
-      <ImageFive />
-      <TextBox6 />
-      <ImageSix />
-      <TextBox5 />
+    
+      <Croatia />
     </div>
   );
 }
---
+++ travel/src/components/Croatia.js
@@ -0,0 +1,34 @@
+import ImageFive from "./ImageFive"
+import ImageFour from "./ImageFour"
+import ImageOne from "./ImageOne"
+import ImageSix from "./ImageSix"
+import ImageThree from "./ImageThree"
+import ImageTwo from "./ImageTwo"
+import { TextBox } from "./TextBox"
+import { TextBox3 } from "./TextBox3"
+import { TextBox4 } from "./TextBox4"
+import { TextBox5 } from "./TextBox5"
+import { TextBox6 } from "./TextBox6"
+import { TextBox2 } from "./Textbox2"
+
+
+function Croatia() {
+  return (
+    <>
+     <ImageOne />
+      <TextBox />
+      <ImageTwo />
+      <TextBox2 />
+      <ImageThree />
+      <TextBox3 />
+      <ImageFour />
+      <TextBox4 />
+      <ImageFive />
+      <TextBox6 />
+      <ImageSix />
+      <TextBox5 />
+    </>
+  )
+}
+
+export default Croatia
